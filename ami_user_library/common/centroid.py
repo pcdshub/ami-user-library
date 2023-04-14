8b16a2d5411729e861337232a3e024ee2a5b53cc
Vincent Esposito <espov@slac.stanford.edu>
2023-04-14 09:39:57 -0700

    MNT: flake8

--- ami_user_library/common/centroid.py
+++ ami_user_library/common/centroid.py
@@ -1,17 +1,13 @@
 
-from ami.flowchart.Node import Node
-from ami.flowchart.library.common import CtrlNode
-import typing
-import amitypes
 import ami.graph_nodes as gn
-
-
+import amitypes
 import numpy as np
+from ami.flowchart.library.common import CtrlNode
 
 
 # ##### utility functions #####
 def remove_bkg(img, n1, n2):
-    img -= ( img[:n1, :n1].mean() + img[-n2:, -n2:].mean() )/2
+    img -= (img[:n1, :n1].mean() + img[-n2:, -n2:].mean())/2
     return img
 
 
@@ -23,32 +19,32 @@ def stats_from_moments(x, y=None):
     """
     if y is None:
         y = np.ones_like(x)
-    y = np.abs(y) #  negative values in the baseline screw things up
+    y = np.abs(y)  # negative values in the baseline screw things up
     mean = np.sum(x*y) / np.sum(y)
-    variance = np.sum( (x-mean)**2*y ) / y.sum()
+    variance = np.sum((x-mean)**2*y) / y.sum()
     sigma = np.sqrt(variance)
-    skew = np.sum( (x-mean)**3*y ) / sigma**3
+    skew = np.sum((x-mean)**3*y) / sigma**3
     return mean, sigma, skew
 
 
 def moment_img(img, m, n):
     """
     Calculate the m, n moment of an image.
-    
+
     img : input array
     m, n: moment order on i,j respectively
     """
     si, sj = img.shape
     ii = np.arange(si)
     jj = np.arange(sj)
-    M_mn = np.dot( ii**m, np.dot(jj**n, img.T) )
+    M_mn = np.dot(ii**m, np.dot(jj**n, img.T))
     return M_mn
 
 
 def central_moment_img(img, centroid, m, n):
     """
     Calculate the m, n central moment of an image.
-    
+
     img : input array
     centroid : centroid coordinate (i,j)
     m, n: moment order on i,j respectively
@@ -57,20 +53,20 @@ def central_moment_img(img, centroid, m, n):
     si, sj = img.shape
     ii = np.arange(si)
     jj = np.arange(sj)
-    Mc_mn = np.dot( (ii-ci)**m, np.dot( (jj-cj)**n,img.T ) )
+    Mc_mn = np.dot((ii-ci)**m, np.dot((jj-cj)**n, img.T))
     return Mc_mn
 
 
 def centroid_from_moments(m_00, m_10, m_01):
-    centroid = ( m_10/m_00, m_01/m_00 )
+    centroid = (m_10/m_00, m_01/m_00)
     return centroid
 
 
 def covariance_from_moments(centroid, m_00, mc_20, mc_11, mc_02):
     ci, cj = centroid
     cov = np.array([
-        [ mc_20/m_00-ci**2, mc_11/m_00-ci*cj ],
-        [ mc_11/m_00-ci*cj, mc_02/m_00-cj**2 ]
+        [mc_20/m_00-ci**2, mc_11/m_00-ci*cj],
+        [mc_11/m_00-ci*cj, mc_02/m_00-cj**2]
     ])
     return cov
 
@@ -80,17 +76,16 @@ def image_stats(img):
     m10 = moment_img(img, 1, 0)
     m01 = moment_img(img, 0, 1)
     centroid = centroid_from_moments(m00, m10, m01)
-    
+
     mc_20 = central_moment_img(img, centroid, 2, 0)
     mc_11 = central_moment_img(img, centroid, 1, 1)
     mc_02 = central_moment_img(img, centroid, 0, 2)
     cov = covariance_from_moments(centroid, m00, mc_20, mc_11, mc_02)
-    
+
     return centroid, cov
 
 
 def image_stats_projection(img):
-    
     centroid = 0
     sigma = 0
     return centroid, sigma
@@ -118,13 +113,13 @@ class EventProcessor():
         pass
 
     def on_event(self, img, *args, **kwargs):
-        use_proj = bool(self.use_proj) 
+        use_proj = bool(self.use_proj)
         img = remove_bkg(img, 10, 10)
-        
+
         if use_proj:
             i_idx = np.arange(img.shape[0])
             com_i, sig_i, skw_i = stats_from_moments(i_idx, y=img.sum(axis=1))
-            
+
             j_idx = np.arange(img.shape[1])
             com_j, sig_j, skw_j = stats_from_moments(j_idx, y=img.sum(axis=0))
             fwhm_i, fwhm_j = (2.355*sig_i, 2.355*sig_j)
@@ -133,7 +128,7 @@ class EventProcessor():
             fwhm_i = 0
             fwhm_j = 0
             com_i, com_j = com
-            
+
         return com_i, com_j, fwhm_i, fwhm_j
 
 
@@ -157,16 +152,15 @@ class centroid(CtrlNode):
 
     def __init__(self, name):
         super().__init__(
-            name, 
+            name,
             terminals={
                 'img': {'io': 'in', 'removable': True, 'ttype': amitypes.array.Array2d, 'optional': False, 'group': None},
                 'com_i': {'io': 'out', 'removable': True, 'ttype': float, 'optional': False, 'group': None},
                 'com_j': {'io': 'out', 'removable': True, 'ttype': float, 'optional': False, 'group': None},
                 'fwhm_i': {'io': 'out', 'removable': True, 'ttype': float, 'optional': False, 'group': None},
-                'fwhm_j': {'io': 'out', 'removable': True, 'ttype': float, 'optional': False, 'group': None}, 
+                'fwhm_j': {'io': 'out', 'removable': True, 'ttype': float, 'optional': False, 'group': None},
             }
         )
-
 
     def to_operation(self, **kwargs):
         proc = EventProcessor()
@@ -178,4 +172,3 @@ class centroid(CtrlNode):
                       end_run=proc.end_run,
                       begin_step=proc.begin_step,
                       end_step=proc.end_step)
-        
